bedc6a25b967d9433c6be52bade3d71a40892b5b
[sora] Takumi Kaneko <sortkane@mediba.jp>
2017-12-26 12:13:21 +0900

    途中経過

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
 # coding: utf-8
 
 import boto3
-
+from elasticsearch import Elasticsearch
 
 
 def lambda_handler(event, context):
     pass
     print('test')
+    es = Elasticsearch('search-cloudtrail-log-hjunjjhcomsxagtvy7chbmsyta.ap-northeast-1.es.amazonaws.com', port=80)
+    print(es.search(index='cwl-2017.12.01'))
+
+
+
+
+
+
+
+
 lambda_handler(1,1)
+
+
+
